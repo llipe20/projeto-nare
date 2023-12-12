57d2c704d610197ccd6125303b3fe0e266f1607b
Felipe <silvahlipe@gmail.com>
2023-12-11 21:54:45 -0300

    commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # projeto-nare
- Meu portfólio kk
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex justify-center items-center w-screen h-auto bg-red-500">
+        <h1 class="font-bold">oIE VOM, FOHSD </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'App',
+
+    data() {
+        return {
+          
+        }
+    },
+
+    methods : {
+
+    }
+}
+</script>
